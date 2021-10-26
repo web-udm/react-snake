dbc81d67f9933e19bbddae480d7e5c249c1f208e
Ildar Mosnabeev <web.udm@gmail.com>
2021-10-26 12:56:08 +0400

    Каркас для игры

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="game">
+            <Board/>
+        </div>
+    );
+}
+
+function Board() {
+    const [head, setHead] = useState(1);
+    const [direction, setDirection] = useState('left');
+    const squares = Array(100).fill('').map(
+        (item, index) => <Square num={index + 1} head={index === head} />
+    );
+
+    function headAction(num) {
+        if (++num > 100) {
+            num = 1;
+        }
+
+        return num;
+    }
+
+    useEffect(() => {
+        setTimeout(() => setHead(headAction(head)), 100)
+    });
+
+    return (
+        <div className="game__board">
+            {squares}
+        </div>
+    );
+}
+
+function Square(props) {
+    const className = `game__square ${props.head ? 'game__square--head' : ''}`;
+
+    return <div className={className} data-index={props.num}></div>
 }
 
 export default App;
